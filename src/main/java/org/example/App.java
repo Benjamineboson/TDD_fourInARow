36f9c41ca2ab1f689ecafc60365903fdfe75d29c
Benjamin E Boson <Benjamineboson@gmail.com>
2020-11-24 21:01:00 +0100

    Init commit with empty board

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        List<String> winningMoves = new ArrayList<>();
+        String [][] arr = new String[6][7];
+
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[i].length ; j++) {
+                arr[i][j] = ""+i+":"+j;
+                winningMoves.add(arr[i][j]);
+            }
+        }
+
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.print("| ");
+            for (int j = 0; j < arr[i].length ; j++) {
+                System.out.print(arr[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------------------------------------");
+        }
+
+////        for (int i = 0; i < 42; i++) {
+////            for (int j = 0; j < ; j++) {
+////
+////            }
+////        }
+//        System.out.println( "Hello World!" );
+    }
+}
