32c356b5b6ca0acabe4eab8a1d48dbf783f9dba7
Benjamin E Boson <Benjamineboson@gmail.com>
2020-11-25 11:24:28 +0100

    Add layouts

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,9 +3,6 @@ package org.example;
 
 import org.example.game_engine.GameBoard;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Hello world!
@@ -16,21 +13,42 @@ public class App
 
     public static void main( String[] args )
     {
-        GameBoard gameBoard = new GameBoard();
-        String [][] arr = new String[6][7];
-        for (int i = 0; i < 7; i++) {
-            if (i%2 == 0) arr = gameBoard.makeAMove(0);
-            else arr = gameBoard.makeAMove(1);
-        }
-        System.out.println(gameBoard.checkWinner());
-        for (int i = arr.length-1; i >= 0; i--) {
-            System.out.print("| ");
-            for (int j = 0; j < arr[i].length ; j++) {
-                System.out.print(arr[i][j] + " | ");
+            GameBoard gameBoard = new GameBoard();
+            String [][] arr = new String[6][7];
+            for (int i = 0; i < 7; i++) {
+                if (i%2 == 0) arr = gameBoard.makeAMove(0);
+                else arr = gameBoard.makeAMove(1);
             }
-            System.out.println();
-            System.out.println("-----------------------------");
-        }
-    }
+            System.out.println(gameBoard.checkWinner());
+            for (int i = arr.length-1; i >= 0; i--) {
+                System.out.print("| ");
+                for (int j = 0; j < arr[i].length ; j++) {
+                    System.out.print(arr[i][j] + " | ");
+                }
+                System.out.println();
+                System.out.println("-----------------------------");
+            }
 
+            System.out.println("┌───┬───┬───┬───┬───┬───┬───┐");
+            for (int i = arr.length-1; i >= 0; i--) {
+                System.out.print("│ ");
+                for (int j = 0; j < arr[i].length ; j++) {
+                    System.out.print(arr[i][j] + " │ ");
+                }
+                System.out.println();
+                if (i >= 1) {
+                    System.out.println("├───┼───┼───┼───┼───┼───┼───┤");
+                } else {
+                    System.out.println("└───┴───┴───┴───┴───┴───┴───┘");
+                }
+            }
+
+            String anotherBoardStyle =
+                    "╔═══╦═══╦═══╦═══╦═══╦═══╦═══╗\n" +
+                    "║   ║ X ║   ║   ║   ║   ║   ║\n" +
+                    "╠═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n" +
+                    "║   ║ X ║ O ║   ║   ║   ║   ║\n" +
+                    "╚═══╩═══╩═══╩═══╩═══╩═══╩═══╝\n";
+            System.out.println(anotherBoardStyle);
+        }
 }
